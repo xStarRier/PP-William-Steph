54a306efea9b4d22194d5ed35641be9a64feeb65
William B <william.buwembo21@gmail.com>
2024-12-04 12:57:07 +0100

    start

--- BokFilm-info/app.cs
+++ BokFilm-info/app.cs
@@ -8,5 +8,18 @@ namespace BokFilm_info
 {
     internal class Class1
     {
+
+        public void Run()
+        {
+            Console.WriteLine("1. See movie list");
+            Console.WriteLine("2. Add movie");
+            Console.WriteLine("3. Quit");
+            var input = Console.ReadLine();
+
+            switch (input)
+            { 
+            
+            }
+        }
     }
 }
--- BokFilm-info/movie.cs
+++ BokFilm-info/movie.cs
@@ -8,5 +8,16 @@ namespace BokFilm_info
 {
     internal class Movie
     {
+        string _movie {  get; private set; }
+        string _year { get; private set; }
+        string _genre { get; private set }
+
+        public Movie(string movie, string year, string genre)
+        {
+            _movie = movie;
+            _year = year;
+            _genre = genre;
+        }
+
     }
 }
